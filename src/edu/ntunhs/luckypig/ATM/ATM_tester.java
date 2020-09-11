b1984e1c23823190bf7d255f8789095952765d4e
郵差 <a34007024@gmail.com>
2020-09-11 23:00:12 +0800

    Create ATM_tester.java

---
+++ src/edu/ntunhs/luckypig/ATM/ATM_tester.java
@@ -0,0 +1,8 @@
+package edu.ntunhs.luckypig.ATM;
+
+public class ATM_tester {
+    public static void main(String[] args) throws InterruptedException {
+        ATM atm = new ATM();
+        atm.ATM_Main_Page();
+    }
+}
