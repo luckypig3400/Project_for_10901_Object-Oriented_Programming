f270f5e589a06854dfb019452c721ac1beb230f7
郵差 <a34007024@gmail.com>
2020-09-11 23:08:22 +0800

    auto Demo deposit and withdraw function

--- src/edu/ntunhs/luckypig/ATM/ATM_tester.java
+++ src/edu/ntunhs/luckypig/ATM/ATM_tester.java
@@ -4,5 +4,13 @@ public class ATM_tester {
     public static void main(String[] args) throws InterruptedException {
         ATM atm = new ATM();
         atm.ATM_Main_Page();
+        Thread.sleep(900);
+        System.out.println("即將進入加分測試項目:");
+        System.out.println("演示自動存入3000元......");
+        atm.deposit(false, 3000);
+        System.out.println("===========================================");
+        Thread.sleep(900);
+        System.out.println("演示自動提款3000元......");
+        atm.withdraw(false, 3000);
     }
 }
