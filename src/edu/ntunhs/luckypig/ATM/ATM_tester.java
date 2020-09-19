62d1e8c8ce0ec15f8379ee5c0b1d319a393d388a
郵差 <a34007024@gmail.com>
2020-09-19 10:02:11 +0800

    add Tread sleep before auto Demo

--- src/edu/ntunhs/luckypig/ATM/ATM_tester.java
+++ src/edu/ntunhs/luckypig/ATM/ATM_tester.java
@@ -6,6 +6,7 @@ public class ATM_tester {
         atm.ATM_Main_Page();
         Thread.sleep(900);
         System.out.println("即將進入加分測試項目:");
+        Thread.sleep(900);
         System.out.println("演示自動存入3000元......");
         atm.deposit(false, 3000);
         System.out.println("===========================================");
