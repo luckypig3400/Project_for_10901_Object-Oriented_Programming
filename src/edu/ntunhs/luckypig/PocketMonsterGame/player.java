cb524cacc6e586cb9a5702649ff9b30414cf3eb2
郵差 <a34007024@gmail.com>
2020-10-16 18:00:23 +0800

    player class idea

---
+++ src/edu/ntunhs/luckypig/PocketMonsterGame/player.java
@@ -0,0 +1,12 @@
+package edu.ntunhs.luckypig.PocketMonsterGame;
+
+public class player {
+    String name;
+    player(String in_name){
+
+    }
+
+    /*
+    構想:讓玩家可以有多隻寶可夢、可以選擇要使用哪隻寶可夢並且可以讓玩家選擇要讓寶可夢發動的技能
+    */
+}
