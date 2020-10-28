84e681b0c60e662db840c1fcf59a064b1fdb49d8
郵差 <a34007024@gmail.com>
2020-10-28 22:06:33 +0800

    update attack func and redefine attack rules

--- src/edu/ntunhs/luckypig/PocketMonsterGame/PocketMonster.java
+++ src/edu/ntunhs/luckypig/PocketMonsterGame/PocketMonster.java
@@ -16,9 +16,9 @@ public class PocketMonster {
 
     PocketMonster() {
         Random setValueRandomMachine = new Random();
-        blood = setValueRandomMachine.nextInt(300) + 600;// by default every monster will have at least
-        attack = setValueRandomMachine.nextInt(100) + 100;
-        defence = setValueRandomMachine.nextInt(30) + 66;
+        blood = setValueRandomMachine.nextInt(300) + 600;// by default every monster will have at least 600 HP
+        attack = setValueRandomMachine.nextInt(100) + 200;// by default every monster will have at least 200 ATK
+        defence = setValueRandomMachine.nextInt(30) + 66;// by default every monster will have at least 66 DEF
         Date systemDate = new Date();
         name = systemDate.toString();// 未命名寶可夢則使用日期命名
     }
@@ -27,8 +27,8 @@ public class PocketMonster {
         name = in_name;
         Random setValueRandomMachine = new Random();
         blood = setValueRandomMachine.nextInt(300) + 600;// by default every monster will have at least 600 HP
-        attack = setValueRandomMachine.nextInt(100) + 100;
-        defence = setValueRandomMachine.nextInt(30) + 66;
+        attack = setValueRandomMachine.nextInt(100) + 200;// by default every monster will have at least 200 ATK
+        defence = setValueRandomMachine.nextInt(30) + 66;// by default every monster will have at least 66 DEF
     }
 
     PocketMonster(String in_name, int in_hp, int in_atk, int in_def, String in_type, float in_wallet, float in_exp) {
@@ -94,21 +94,60 @@ public class PocketMonster {
     }
 
     void Attack(PocketMonster enemyMonster) {
-        //27%機率自身攻擊兩倍，9%機率自身攻擊減半，3%打出致命一擊(扣除對方當前血量的9/10)，其餘機率扣除對方迴避率後的攻擊力不變
+        // 沒有被閃避成功發動攻擊時，有36%機率自身攻擊兩倍，11%機率自身攻擊減半，3%打出致命一擊(扣除對方當前血量的9/10)
 
         Random rnd = new Random();
-        if (rnd.nextInt(101) > enemyMonster.avoidRate) {// 發動攻擊
-            enemyMonster.blood -= (attack - enemyMonster.defence);// 先扣除敵人防禦值再打
-            if (enemyMonster.blood > 0)
-                System.out.println(name + "發動了普通攻擊對敵人造成了" + (attack - enemyMonster.defence) + "點傷害\t"
-                        + enemyMonster.name + "的血量剩餘:" + enemyMonster.blood);
-            else {
-                System.out.println(enemyMonster.name + "已經被" + name + "擊敗了!");
-                exp += enemyMonster.exp;// kill the enemy get EXP
-                System.out.println(name + "增加了" + exp + "點經驗");
+        int randomNumber = rnd.nextInt(101);
+        if (randomNumber > enemyMonster.avoidRate) {// 沒有被閃避，正常發動攻擊
+            randomNumber = rnd.nextInt(101);
+            if (randomNumber > 97) {// 致命一擊
+                enemyMonster.blood -= enemyMonster.blood*9/10;// 無視防禦直接扣除敵方現有的9/10血量
+                if (enemyMonster.blood > 0)
+                    System.out.println(name + "全神貫注打出了致命一擊!!!!!!無視防禦直接扣除敵方現有的9/10血量!!!"
+                            + enemyMonster.name + "的血量剩餘:" + enemyMonster.blood);
+                else {
+                    System.out.println(enemyMonster.name + "已經被" + name + "擊敗了!");
+                    exp += enemyMonster.exp;// kill the enemy get EXP
+                    System.out.println(name + "增加了" + exp + "點經驗");
+                }
+            } else if (randomNumber > 86) {// 攻擊力減半
+                enemyMonster.blood -= (attack/2 - enemyMonster.defence);// 先扣除敵人防禦值再打
+                if (enemyMonster.blood > 0)
+                    System.out.println(name + "這回合虛弱造成攻擊力減半(*﹏*)對敵人造成了" + (attack/2 - enemyMonster.defence) + "點傷害\t"
+                            + enemyMonster.name + "的血量剩餘:" + enemyMonster.blood);
+                else {
+                    System.out.println(enemyMonster.name + "已經被" + name + "擊敗了!");
+                    exp += enemyMonster.exp;// kill the enemy get EXP
+                    System.out.println(name + "增加了" + exp + "點經驗");
+                }
+            } else if (randomNumber > 50) {// 攻擊力兩倍
+                enemyMonster.blood -= (2 * attack - enemyMonster.defence);// 先扣除敵人防禦值再打
+                if (enemyMonster.blood > 0)
+                    System.out.println(name + "這回合氣宇軒昂鬥志充沛造成攻擊力翻倍!對敵人造成了" + (2 * attack - enemyMonster.defence) + "點傷害\t"
+                            + enemyMonster.name + "的血量剩餘:" + enemyMonster.blood);
+                else {
+                    System.out.println(enemyMonster.name + "已經被" + name + "擊敗了!");
+                    exp += enemyMonster.exp;// kill the enemy get EXP
+                    System.out.println(name + "增加了" + exp + "點經驗");
+                }
+            } else {// 普攻
+                enemyMonster.blood -= (attack - enemyMonster.defence);// 先扣除敵人防禦值再打
+                if (enemyMonster.blood > 0)
+                    System.out.println(name + "發動了普通攻擊對敵人造成了" + (attack - enemyMonster.defence) + "點傷害\t"
+                            + enemyMonster.name + "的血量剩餘:" + enemyMonster.blood);
+                else {
+                    System.out.println(enemyMonster.name + "已經被" + name + "擊敗了!");
+                    exp += enemyMonster.exp;// kill the enemy get EXP
+                    System.out.println(name + "增加了" + exp + "點經驗");
+                }
             }
-        } else // 被躲掉了
+
+        } else{ // 被躲掉了
             System.out.println(name + "的攻擊被" + enemyMonster.name + "躲掉啦(*﹏*)");
+        }
+        
+        enemyMonster.avoidRate = 9;//不管是否打中，重置敵人的閃避率為9%
+
     }
 
     void Defense() {// make pokemon's avoidRate = 30%
