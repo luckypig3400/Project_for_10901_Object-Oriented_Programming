8ddc6f2fa19024c9fc542136aaba5602fbbdb3ef
郵差 <a34007024@gmail.com>
2020-10-30 18:06:11 +0800

    random  decide who goes first in this round
    為了確保公平性
    隨機抽取每回合的行動優先序

--- src/edu/ntunhs/luckypig/PocketMonsterGame/monsterGame.java
+++ src/edu/ntunhs/luckypig/PocketMonsterGame/monsterGame.java
@@ -1,5 +1,7 @@
 package edu.ntunhs.luckypig.PocketMonsterGame;
 
+import java.util.Random;
+
 public class monsterGame {
     public static void main(String[] args) throws InterruptedException {
 
@@ -29,18 +31,34 @@ public class monsterGame {
             turtle.selectPocketMonsterSkill();//玩家2
             Thread.sleep(600);
 
-            System.out.println("玩家1的回合由"+PikaChu.name+"行動:");
+            System.out.println("正在隨機抽取誰優先行動");
             Thread.sleep(600);
-            PikaChu.usePocketMonsterSkill(turtle);
-            Thread.sleep(900);
+            Random rnd = new Random();
+            int whoGoFirst = rnd.nextInt(2);
+            if(whoGoFirst == 0){
+                System.out.println("由"+PikaChu.name+"先行動:");
+                PikaChu.usePocketMonsterSkill(turtle);
+                Thread.sleep(900);
+            }else{
+                System.out.println("由"+turtle.name+"先行動:");
+                turtle.usePocketMonsterSkill(PikaChu);
+                Thread.sleep(900);
+            }
             
             if(PikaChu.blood <= 0 || turtle.blood <= 0) break;//break判斷寫在中間可以防止死亡的Pokemon多打一次 by 柏勳
-
-            System.out.println("玩家2的回合由"+turtle.name+"行動:");
-            Thread.sleep(600);
-            turtle.usePocketMonsterSkill(PikaChu);
-            Thread.sleep(900);
             
+            if(whoGoFirst == 0){
+                System.out.println("接著由"+turtle.name+"行動:");
+                turtle.usePocketMonsterSkill(PikaChu);
+                Thread.sleep(900);
+            }else{
+                System.out.println("接著由"+PikaChu.name+"行動:");
+                PikaChu.usePocketMonsterSkill(turtle);
+                Thread.sleep(900);
+            }
+            
+            if(PikaChu.blood <= 0 || turtle.blood <= 0) break;//雙方都攻擊後再次確認雙方血量
+
             System.out.println("======第"+roundCount+"回合結束======\n\n\n");
             roundCount += 1;
             Thread.sleep(600);
