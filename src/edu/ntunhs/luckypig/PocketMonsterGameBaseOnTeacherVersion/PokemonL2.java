59a9e9ef7f8f6e82d546417a4bff33cf1833dcc4
郵差 <a34007024@gmail.com>
2020-12-05 00:05:11 +0800

    fix evolution logic bug in L2 and L3

--- src/edu/ntunhs/luckypig/PocketMonsterGameBaseOnTeacherVersion/PokemonL2.java
+++ src/edu/ntunhs/luckypig/PocketMonsterGameBaseOnTeacherVersion/PokemonL2.java
@@ -4,21 +4,16 @@ public class PokemonL2 extends Pokemon {
 
     int oHP, oATK, oDEF, oAvoidRate;// o stand for original
 
-    PokemonL2() {
-        super();
-        oHP = HP;
-        oATK = attack;
-        oDEF = defense;
-        oAvoidRate = avoidRate;
-        evolution();
-    }
+    //因為是用來進化的，所以應該只能透過有提供完整資料的建構子來進行
+    //(不可以再像剛生成時使用亂數來賦予數值)
 
-    PokemonL2(int in_ID, String in_Name) {
-        super(in_ID, in_Name);
-        oHP = HP;
-        oATK = attack;
-        oDEF = defense;
-        oAvoidRate = avoidRate;
+    PokemonL2(int in_ID, String in_Name, int in_HP, int in_ATK, int in_DEF, int in_avoidRate) {
+        ID = in_ID;
+        Name = in_Name;
+        oHP = in_HP;
+        oATK = in_ATK;
+        oDEF = in_DEF;
+        oAvoidRate = in_avoidRate;
         evolution();
     }
 
--- src/edu/ntunhs/luckypig/PocketMonsterGameBaseOnTeacherVersion/PokemonL3.java
+++ src/edu/ntunhs/luckypig/PocketMonsterGameBaseOnTeacherVersion/PokemonL3.java
@@ -2,20 +2,26 @@ package edu.ntunhs.luckypig.PocketMonsterGameBaseOnTeacherVersion;
 
 public class PokemonL3 extends PokemonL2 {
 
-    PokemonL3() {
-        super();
-    }
+    //因為是用來進化的，所以應該只能透過有提供完整資料的建構子來進行
+    //(不可以再像剛生成時使用亂數來賦予數值)
 
-    PokemonL3(int in_ID, String in_Name) {
-        super(in_ID, in_Name);
+    PokemonL3(int in_ID, String in_Name, int in_HP, int in_ATK, int in_DEF, int in_avoidRate) {
+        super(in_ID, in_Name, in_HP, in_ATK, in_DEF, in_avoidRate);
+        ID = in_ID;
+        Name = in_Name;
+        oHP = in_HP;
+        oATK = in_ATK;
+        oDEF = in_DEF;
+        oAvoidRate = in_avoidRate;
+        evolution();
     }
 
     @Override
     void evolution() {
-        HP *= 1.2;
-        attack *= 1.2;
-        defense *= 1.2;
+        HP *= 1.3;
+        attack *= 1.3;
+        defense *= 1.3;
         avoidRate += 9;//最終型態的起始閃避率提升了不少
     }
-    
+
 }
