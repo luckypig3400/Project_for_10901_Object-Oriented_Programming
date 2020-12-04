c5c3661f7291e1dc195f8add52e663a430cae19e
郵差 <a34007024@gmail.com>
2020-12-04 18:48:31 +0800

    add defaultPaymentOption and related function

--- src/edu/ntunhs/luckypig/PolymorphismExample20201204ATM/NTUNHSpay.java
+++ src/edu/ntunhs/luckypig/PolymorphismExample20201204ATM/NTUNHSpay.java
@@ -1,13 +1,12 @@
 package edu.ntunhs.luckypig.PolymorphismExample20201204ATM;
 
-import javax.lang.model.util.ElementScanner6;
-
 /* 電子錢包付費功能範例 */
 
 public class NTUNHSpay {
     ATM eWalletPaymentOption1;
     ATM eWalletPaymentOption2;
     private int Bonus;
+    int defaultPaymentOption;
 
     NTUNHSpay() {
         eWalletPaymentOption1 = new NetworkATM();
@@ -15,18 +14,40 @@ public class NTUNHSpay {
         // 父類別ATM可以使用子類別NetworkATM的方法與建構子
         // eWallet的資料型態為ATM 使用的建構子卻是 NetworkATM的建構子
         // 這樣的用法稱之為多型(Polymorphism)
+        eWalletPaymentOption1.SetBalance(6000);
+        eWalletPaymentOption2.SetBalance(6000);
         Bonus = 0;
+        defaultPaymentOption = 1;//預設使用網銀付款
     }
 
-    int payment(int paymentAmount, int paymentOption) {
-        if (paymentOption == 1) {
+    int payment(int paymentAmount, int in_paymentOption) {
+        if (in_paymentOption == 1) {
             if (eWalletPaymentOption1.withdraw(paymentAmount) == 1) {
                 System.out.println("電子付款成功~! ");
                 return 1;
             } else {
                 return 0;
             }
-        } else if (paymentOption == 2) {
+        } else if (in_paymentOption == 2) {
+            if (eWalletPaymentOption2.withdraw(paymentAmount) == 1) {
+                System.out.println("電子付款成功~! ");
+                return 1;
+            } else {
+                return 0;
+            }
+        } else
+            return 0;
+    }
+
+    int payment(int paymentAmount) {
+        if (defaultPaymentOption == 1) {
+            if (eWalletPaymentOption1.withdraw(paymentAmount) == 1) {
+                System.out.println("電子付款成功~! ");
+                return 1;
+            } else {
+                return 0;
+            }
+        } else if (defaultPaymentOption== 2) {
             if (eWalletPaymentOption2.withdraw(paymentAmount) == 1) {
                 System.out.println("電子付款成功~! ");
                 return 1;
@@ -42,10 +63,19 @@ public class NTUNHSpay {
         return 0;
     }
 
-    int getBalance(int paymentOption) {
-        if (paymentOption == 1) {
+    int getBalance(int in_paymentOption) {
+        if (in_paymentOption == 1) {
             return eWalletPaymentOption1.getBalance();
-        } else if (paymentOption == 1) {
+        } else if (in_paymentOption == 2) {
+            return eWalletPaymentOption2.getBalance();
+        } else
+            return 0;
+    }
+
+    int getBalance() {
+        if (defaultPaymentOption == 1) {
+            return eWalletPaymentOption1.getBalance();
+        } else if (defaultPaymentOption == 2) {
             return eWalletPaymentOption2.getBalance();
         } else
             return 0;
