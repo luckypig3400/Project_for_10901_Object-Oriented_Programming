9b16c5099c549756d34be1401a6e7435453cc01e
郵差 <a34007024@gmail.com>
2020-12-05 00:24:09 +0800

    adjust L3 evolution value

--- src/edu/ntunhs/luckypig/PocketMonsterGameBaseOnTeacherVersion/PokemonL3.java
+++ src/edu/ntunhs/luckypig/PocketMonsterGameBaseOnTeacherVersion/PokemonL3.java
@@ -7,20 +7,21 @@ public class PokemonL3 extends PokemonL2 {
 
     PokemonL3(int in_ID, String in_Name, int in_HP, int in_ATK, int in_DEF, int in_avoidRate) {
         super(in_ID, in_Name, in_HP, in_ATK, in_DEF, in_avoidRate);
+        //呼叫super()會使用PokemonL2的進化函式先進化一次
         ID = in_ID;
         Name = in_Name;
         oHP = in_HP;
         oATK = in_ATK;
         oDEF = in_DEF;
         oAvoidRate = in_avoidRate;
-        evolution();
+        evolution();//後面再使用覆寫過後的進化再次更改數值
     }
 
     @Override
     void evolution() {
-        HP *= 1.3;
-        attack *= 1.3;
-        defense *= 1.3;
+        HP *= 1.1;
+        attack *= 1.1;
+        defense *= 1.1;
         avoidRate += 9;//最終型態的起始閃避率提升了不少
     }
 
