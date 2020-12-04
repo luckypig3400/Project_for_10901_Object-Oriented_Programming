7796e83806a85018a7a283b878bcc6fa364017d9
郵差 <a34007024@gmail.com>
2020-12-04 18:24:07 +0800

    add new payment method in NTUNHSPay

--- src/edu/ntunhs/luckypig/PolymorphismExample20201204ATM/NTUNHSpay.java
+++ src/edu/ntunhs/luckypig/PolymorphismExample20201204ATM/NTUNHSpay.java
@@ -1,28 +1,40 @@
 package edu.ntunhs.luckypig.PolymorphismExample20201204ATM;
 
+import javax.lang.model.util.ElementScanner6;
+
 /* 電子錢包付費功能範例 */
 
 public class NTUNHSpay {
-    ATM eWallet;
+    ATM eWalletPaymentOption1;
+    ATM eWalletPaymentOption2;
     private int Bonus;
 
     NTUNHSpay() {
-        //eWallet = new NetworkATM();
-        eWallet = new creditCard();
+        eWalletPaymentOption1 = new NetworkATM();
+        eWalletPaymentOption2 = new creditCard();
         // 父類別ATM可以使用子類別NetworkATM的方法與建構子
         // eWallet的資料型態為ATM 使用的建構子卻是 NetworkATM的建構子
         // 這樣的用法稱之為多型(Polymorphism)
-        eWallet.SetBalance(6000);
         Bonus = 0;
     }
 
-    int payment(int paymentAmount) {
-        if (eWallet.withdraw(paymentAmount) == 1) {
-            System.out.println("電子付款成功~! ");
-            return 1;
-        } else {
+    int payment(int paymentAmount, int paymentOption) {
+        if (paymentOption == 1) {
+            if (eWalletPaymentOption1.withdraw(paymentAmount) == 1) {
+                System.out.println("電子付款成功~! ");
+                return 1;
+            } else {
+                return 0;
+            }
+        } else if (paymentOption == 2) {
+            if (eWalletPaymentOption2.withdraw(paymentAmount) == 1) {
+                System.out.println("電子付款成功~! ");
+                return 1;
+            } else {
+                return 0;
+            }
+        } else
             return 0;
-        }
     }
 
     int Register(int openDepositAmount) {
@@ -30,11 +42,16 @@ public class NTUNHSpay {
         return 0;
     }
 
-    int getBalance() {
-        return eWallet.getBalance();
+    int getBalance(int paymentOption) {
+        if (paymentOption == 1) {
+            return eWalletPaymentOption1.getBalance();
+        } else if (paymentOption == 1) {
+            return eWalletPaymentOption2.getBalance();
+        } else
+            return 0;
     }
 
-    int getBonus(){
+    int getBonus() {
         return Bonus;
     }
 }
