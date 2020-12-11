45d31b3c72dddfa3701d47e1630596a9f510ed8d
郵差 <a34007024@gmail.com>
2020-12-11 16:40:13 +0800

    try to declare insideInnerClass

--- src/edu/ntunhs/luckypig/Nested_Class_Example.java
+++ src/edu/ntunhs/luckypig/Nested_Class_Example.java
@@ -4,8 +4,10 @@ public class Nested_Class_Example {
     public static void main(String[] args) {
         OuterClass outer1 = new OuterClass();
         OuterClass.InnerClass inner1 = new OuterClass().new InnerClass();
+        OuterClass.InnerClass.insideInnerClass insideInner1 = new OuterClass().new InnerClass().new insideInnerClass();
         outer1.printClassInfo();
         inner1.printClassInfo();
+        insideInner1.printClassInfo();
     }
 }
 
@@ -24,7 +26,7 @@ class OuterClass {
         String classInfo;
 
         InnerClass() {
-            classInfo = "You are in InnerClass";
+            classInfo = "You are in InnerClass(Nested Class Example)";
         }
 
         void printClassInfo() {
@@ -35,7 +37,7 @@ class OuterClass {
             String classInfo;
 
             insideInnerClass() {
-                classInfo = "You are in insideInnerClass";
+                classInfo = "You are in insideInnerClass(Nested Class Example)";
             }
 
             void printClassInfo() {
