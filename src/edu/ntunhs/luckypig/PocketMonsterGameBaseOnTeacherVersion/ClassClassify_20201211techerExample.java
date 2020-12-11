9b19f7c29c9a5b103833ae2c28072d73604df2ec
郵差 <a34007024@gmail.com>
2020-12-11 16:52:14 +0800

    Create ClassClassify_20201211techerExample.java

---
+++ src/edu/ntunhs/luckypig/PocketMonsterGameBaseOnTeacherVersion/ClassClassify_20201211techerExample.java
@@ -0,0 +1,8 @@
+package edu.ntunhs.luckypig.PocketMonsterGameBaseOnTeacherVersion;
+
+/**
+ * ClassClassify_20201211techer's Example
+ */
+public class ClassClassify_20201211techerExample {
+    
+}
