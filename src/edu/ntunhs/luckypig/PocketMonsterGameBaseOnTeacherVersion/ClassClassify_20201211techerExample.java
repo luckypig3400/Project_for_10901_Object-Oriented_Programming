dcecbf4cf670b6e502964248b652c8d9ba6f8a69
郵差 <a34007024@gmail.com>
2020-12-11 16:58:24 +0800

    add classifiedClass structure by teacher

--- src/edu/ntunhs/luckypig/PocketMonsterGameBaseOnTeacherVersion/ClassClassify_20201211techerExample.java
+++ src/edu/ntunhs/luckypig/PocketMonsterGameBaseOnTeacherVersion/ClassClassify_20201211techerExample.java
@@ -4,5 +4,24 @@ package edu.ntunhs.luckypig.PocketMonsterGameBaseOnTeacherVersion;
  * ClassClassify_20201211techer's Example
  */
 public class ClassClassify_20201211techerExample {
-    
+
+}
+
+class Monster {
+    abstract class Pokemon {
+
+    }
+
+    class PokemonLV1 extends Pokemon {
+
+    }
+
+    class PokemonLV2 extends PokemonLV1 implements move {
+
+    }
+
+    interface move {
+        int run(int movement);
+        int fly(int movement);
+    }
 }
