2b2b873295978bca65daef301577ad5808692154
郵差 <a34007024@gmail.com>
2020-12-12 10:40:19 +0800

    copy files into PocketMonsterGameRenew_20201211

---
+++ src/edu/ntunhs/luckypig/PocketMonsterGameRenew_20201211/ATM.java
@@ -0,0 +1,64 @@
+package edu.ntunhs.luckypig.PocketMonsterGameRenew_20201211;
+
+public class ATM {
+    private String ID, Name, PWD;
+    int Balance;
+
+    ATM() { // default Account (for test only)
+        ID = "082214226";
+        Name = "楊曜承";
+        PWD = "226";
+        Balance = 60000;
+    }
+
+    ATM(String in_ID, String in_Name, String in_PWD) {
+        ID = in_ID;
+        Name = in_Name;
+        PWD = in_PWD;
+        Balance = 0;
+    }
+
+    ATM(String in_ID, String in_Name, String in_PWD, int openingDeposit) {
+        ID = in_ID;
+        Name = in_Name;
+        PWD = in_PWD;
+        Balance = openingDeposit;// 開戶存入金額
+    }
+
+    int withdraw(int withdrawAmount) {
+        if (0 > withdrawAmount)// 輸入金額錯誤，不可小於0
+            return 0;
+        if (Balance >= withdrawAmount) {
+            Balance -= withdrawAmount;
+            return 1;
+        } else
+            return 0;
+    }
+
+    void setBalance(int adjustedBalanceAmount) {
+        Balance = adjustedBalanceAmount;
+    }
+
+    int getBalance() {
+        return Balance;
+    }
+
+    String getAccountInfo() {
+        return Name + "您好~ 您的帳號ID:" + ID + "剩餘餘額:" + Balance;
+    }
+
+    void printAccountInfo() {
+        System.out.println(Name + "您好~ 您的帳號ID:" + ID + "剩餘餘額:" + Balance);
+    }
+
+    int login(String in_ID, String in_PWD) {
+        if (in_PWD.equals(PWD)) {
+            return 1;// login successful
+        } else
+            return 0;// login failed
+    }
+
+    void printATMmenu() {
+        
+    }
+}
--- src/edu/ntunhs/luckypig/PocketMonsterGameRenew_20201211/Player.java
+++ src/edu/ntunhs/luckypig/PocketMonsterGameRenew_20201211/Player.java
@@ -1,5 +1,45 @@
 package edu.ntunhs.luckypig.PocketMonsterGameRenew_20201211;
 
 public class Player {
-    
+    private int ID;
+    private String PWD;
+    String Name;
+    int playerPokemonCount = 0;
+    Pokemon[] pokemonInPlayerBag = new Pokemon[300];
+
+    Player(int gID, String gPWD) {
+        ID = gID;
+        PWD = gPWD;
+    }
+
+    boolean setPWD(String oPWD, String gPWD) {
+        if (oPWD.equals(PWD)) {
+            PWD = gPWD;
+            return true;
+        } else
+            return false;
+    }
+
+    String getName() {
+        return Name;
+    }
+
+    void setName(String gName) {
+        Name = gName;
+    }
+
+    int login(int gID, String gPWD) {
+        if (gID != ID) {
+            return 2; // 帳號錯誤
+        }
+        if (!PWD.equals(gPWD)) {
+            return 3; // 密碼錯誤
+        }
+        return 1; // 登入成功
+    }
+
+    void addNewPokemonInPlayerBag(Pokemon newMonster) {
+        pokemonInPlayerBag[playerPokemonCount] = newMonster;
+        playerPokemonCount += 1;
+    }
 }
