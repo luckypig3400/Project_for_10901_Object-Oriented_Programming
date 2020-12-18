83842b5adeaafc87df3fe5b984a48d80509c10f0
郵差 <a34007024@gmail.com>
2020-12-18 17:16:22 +0800

    add TODOs in main Game class

--- src/edu/ntunhs/luckypig/PocketMonsterGameRenew_20201211/Game.java
+++ src/edu/ntunhs/luckypig/PocketMonsterGameRenew_20201211/Game.java
@@ -2,6 +2,10 @@ package edu.ntunhs.luckypig.PocketMonsterGameRenew_20201211;
 
 public class Game {
 
+    // TODO: use ArrayList to store players or pokemons
+    // https://www.javatpoint.com/java-arraylist
+    // TODO: create a class for File Saving (Maybe call it FileSavingjava)
+
     Player p1 = new Player(1, "玩家1", "1");
     Player p2 = new Player(2, "玩家2", "2");
 
