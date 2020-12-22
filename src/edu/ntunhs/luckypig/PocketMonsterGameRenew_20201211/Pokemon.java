6f9928970d00b5dcfe3ea5670f0f8790004e36d0
郵差 <a34007024@gmail.com>
2020-12-22 22:42:22 +0800

    delete abstractPokemon and move variables to Pokemon

--- src/edu/ntunhs/luckypig/PocketMonsterGameRenew_20201211/Pokemon.java
+++ src/edu/ntunhs/luckypig/PocketMonsterGameRenew_20201211/Pokemon.java
@@ -8,6 +8,25 @@ import edu.ntunhs.luckypig.PocketMonsterGameRenew_20201211.RandomString;
 
 public class Pokemon {
 
+    int type;// 寶可夢具有三種型態1、2、3
+    // 1為初始型態、3為最終進化型態
+    int level;// 寶可夢具有等級，其上限為99等
+    int exp;// 使用經驗值來判斷寶可夢等級
+    // 寶可夢被擊敗的話會掉落身上9%經驗值給對方但自身經驗不會減少
+    String attribute = "none";// 寶可夢具有6種屬性
+    // none為無屬性、water為水屬性、fire為火屬性、wood為木屬性、light為光屬性、dark為暗屬性
+    // 日後可以使用屬性來做相剋加減傷
+    int ID;
+    String Name;
+    int currentLocationX, currentLocationY;// 這隻寶可夢在地圖上的位置
+    int HP, ATK, DEF, agile;// 三圍數值 與 敏捷度
+    // 於戰鬥系統中使用寶可夢的敏捷度來決定先攻順序
+    int maxHP;// 使用回血時血量不可超過血量上限
+    double dodgeRate, criticalRate;// 閃避率 與 爆擊率
+    // float and double datatype in Java
+    // https://stackoverflow.com/questions/27598078/float-and-double-datatype-in-java
+    boolean defendStatus;// 玩家選擇防禦後紀錄防禦狀態
+
     interface move { // maybe use it in the map system
         int run(int movementX, int movementY);
 
@@ -20,28 +39,7 @@ public class Pokemon {
         int evolutionToType3(int currentType);
     }
 
-    abstract class abstractPokemon extends Pokemon{
-        int type;// 寶可夢具有三種型態1、2、3
-        // 1為初始型態、3為最終進化型態
-        int level;// 寶可夢具有等級，其上限為99等
-        int exp;// 使用經驗值來判斷寶可夢等級
-        // 寶可夢被擊敗的話會掉落身上9%經驗值給對方但自身經驗不會減少
-        String attribute = "none";// 寶可夢具有6種屬性
-        // none為無屬性、water為水屬性、fire為火屬性、wood為木屬性、light為光屬性、dark為暗屬性
-        // 日後可以使用屬性來做相剋加減傷
-        int ID;
-        String Name;
-        int currentLocationX, currentLocationY;// 這隻寶可夢在地圖上的位置
-        int HP, ATK, DEF, agile;// 三圍數值 與 敏捷度
-        // 於戰鬥系統中使用寶可夢的敏捷度來決定先攻順序
-        int maxHP;// 使用回血時血量不可超過血量上限
-        double dodgeRate, criticalRate;// 閃避率 與 爆擊率
-        // float and double datatype in Java
-        // https://stackoverflow.com/questions/27598078/float-and-double-datatype-in-java
-        boolean defendStatus;// 玩家選擇防禦後紀錄防禦狀態
-    }
-
-    public class PokemonNoneAttribute extends abstractPokemon implements move, evolution {
+    public class PokemonNoneAttribute extends Pokemon implements move, evolution {
         Random rnd = new Random();
         RandomString rndString = new RandomString();
 
